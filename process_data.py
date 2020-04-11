aa0c986000a23db13693f722225164c576987b96
Haroon Ghori <hsghori@gmail.com>
2020-04-11 11:58:22 -0700

    update

--- process_data.py
+++ process_data.py
@@ -6,16 +6,33 @@ import argparse
 
 def main(states=None):
     df = pd.read_csv('covid-19-data/us-states.csv')
+    fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+    longest_dates = []
     for state in states:
         df_state = df[df.state == state]
-        plt.plot(df_state.date, np.log2(df_state.cases), label=state)
-    x_ticks, _ = plt.xticks()
-    plt.xticks(x_ticks[::7], rotation=45)
-    plt.grid(axis='x')
-    plt.title('Covid cases by date')
-    plt.ylabel('Log2 of # of cases')
-    plt.xlabel('Date')
-    plt.legend(loc='best')
+        axes[0].plot(df_state.date, np.log2(df_state.cases), label=state)
+        axes[1].plot(df_state.date, df_state.cases.diff(), label=state)
+        dates = df_state.date.to_numpy()
+        if len(dates) > len(longest_dates):
+            longest_dates = dates
+    x_ticks = list(range(len(longest_dates)))[::7]
+    labels = longest_dates[x_ticks]
+    axes[0].set_xticks(x_ticks)
+    axes[0].set_xticklabels(labels, rotation=45)
+    axes[0].grid(axis='x')
+    axes[0].set_title('Covid cases by date')
+    axes[0].set_ylabel('Log2 of # of cases')
+    axes[0].set_xlabel('Date')
+    axes[0].legend(loc='best')
+
+    axes[1].set_xticks(x_ticks)
+    axes[1].set_xticklabels(labels, rotation=45)
+    axes[1].grid(axis='x')
+    axes[1].set_title('New cases by date')
+    axes[1].set_ylabel('New cases')
+    axes[1].set_xlabel('Date')
+    axes[1].legend(loc='best')
+
     plt.tight_layout()
     plt.savefig('plots/covid_plot.png')
 
